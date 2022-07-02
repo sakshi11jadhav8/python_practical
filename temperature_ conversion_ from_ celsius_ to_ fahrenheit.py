a0dc5f61f2004bf4fb9101347380f875a18e95ce
sakshi11jadhav8 <103136920+sakshi11jadhav8@users.noreply.github.com>
2022-07-03 00:04:05 +0530

    Delete temperature_ conversion_ from_ celsius_ to_ fahrenheit.py

--- temperature_ conversion_ from_ celsius_ to_ fahrenheit.py
+++
@@ -1,5 +0,0 @@
-#Program to Convert a temperature value in celsius to fahrenheits
-
-celsius = int(input("Enter value of temperature in celsius :"))
-fahrenheits = celsius*1.8+32
-print("fahrenheit value of temperature :",fahrenheits)
