4063e3c8653cb36427b2b0c010ed6d34ce726028
sakshi11jadhav8 <103136920+sakshi11jadhav8@users.noreply.github.com>
2022-07-24 21:33:07 +0530

    Update student_details.sql

--- Python Case Study/student_details.sql
+++ Python Case Study/student_details.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `students_details` (
   `PRN` int NOT NULL,
-  `First name` varchar(15) NOT NULL,
-  `Middle name` varchar(15) NOT NULL,
-  `Last name` varchar(15) NOT NULL,
+  `First name` varchar(45) NOT NULL,
+  `Middle name` varchar(45) NOT NULL,
+  `Last name` varchar(45) NOT NULL,
   `Address` varchar(45) NOT NULL,
-  `Mobile no.` varchar(12) NOT NULL,
-  `E-mail` varchar(25) NOT NULL,
+  `Mobile no.` varchar(45) NOT NULL,
+  `E-mail` varchar(45) NOT NULL,
   `Age` int NOT NULL,
   PRIMARY KEY (`PRN`)
 )
